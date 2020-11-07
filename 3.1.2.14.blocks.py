d52071ae884c85046265d909834c0d6cb6733b11
Gersson Tao <gersson.tao@gmail.com>
2020-11-07 13:10:20 -0500

    Piramide
    Laboratorio piramide resuelto

--- 3.1.2.14.blocks.py
+++ 3.1.2.14.blocks.py
@@ -1,16 +1,11 @@
 bloques = int(input("Ingrese el número de bloques: "))
-altura= 1
-capaCompleta = 1
-capaInferior = 0
-capaSuperior = 0
+altura = 0 
 #
 # Escribe tu código aquí.
 #
 i=1
-while i < bloques:
-    print(capaInferior, capaSuperior)
-    capaInferior = capaInferior + 1
-    i += 1
-    print(i)
-    print(capaInferior, capaSuperior)
+while i <= bloques:
+    altura = i 
+    bloques = bloques - i 
+    i+=1
 print("La altura de la pirámide:", altura)
