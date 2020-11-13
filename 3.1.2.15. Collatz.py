6a8c89079dd44cf7a175bef322e0d0a4f4740937
Gersson Tao <gersson.tao@gmail.com>
2020-11-12 20:43:14 -0500

    Collatz

--- 3.1.2.15. Collatz.py
+++ 3.1.2.15. Collatz.py
@@ -1,15 +1,20 @@
 c0=int(input("Ingrese numero: "))
 counter=0
 resultado=0
-while c0!=1:
-    if c0%2==0:
-        resultado= c0//2
-        print(resultado)
-    else:
-        c0=resultado
-        resultado=3*c0+1
-        print(resultado)
-    counter+=1
-    
+while True:
+    while c0!=1:
         
+        if c0%2==0:
+            resultado= c0//2
+            
+            print(resultado)
+            
+        else:
+            resultado=3*c0+1
+            
+            print(resultado)
+            
+        c0=resultado
+        counter+=1
+    break
 print("Pasos: ", counter)
